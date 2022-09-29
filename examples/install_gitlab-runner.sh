528fa8fb91b53005d2d985c2b6d2cca5e0ee4806
websi96 <kontakt@weber-thomas.at>
2022-09-29 15:58:54 +0200

    fix examples to use correct vars

--- examples/install_gitlab-runner.sh
+++ examples/install_gitlab-runner.sh
@@ -2,11 +2,13 @@
 
 helm repo add gitlab https://charts.gitlab.io --force-update 
 
+DEMO_NAME="builder-demo" envsubst < runner.values.yaml > values.yaml
+
 helm upgrade --install \
   runner gitlab/gitlab-runner \
   --namespace builder \
   --create-namespace \
-  --set gitlabUrl: "gitops.wu.ac.at" \
+  --set gitlabUrl: "gitlab.com" \
   --set runnerRegistrationToken: "" \
-  --values runner.values.yaml \
+  --values values.yaml \
   --wait
